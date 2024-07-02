6c8f462d138a2132b13950d6a61620f4e3f18c60
Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
2024-07-02 13:30:19 +0530

    fix the issue where .env becomes empty

--- common/environment/consul-init.sh
+++ common/environment/consul-init.sh
@@ -10,14 +10,18 @@ vault login -method=userpass username=$VAULT_USERNAME password=$VAULT_PASSWORD
 
 export VAULT_TOKEN=$(vault print token)
 
-IFS=$'\n'
-vault kv put kv/env \
-  $(grep -v '^#' .env | grep -e '[^[:space:]]' | \
-    sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | \
-    awk -F '=' '{gsub(/^["'\'']|["'\'']$/, "", $2); data[$1]=$2} END {for (key in data) print key "=" data[key]}')
-    
+export IFS=$'\n'
+if ! vault kv get kv/env >/dev/null 2>&1; then
+  vault kv put kv/env \
+    $(grep -v '^#' .env | grep -e '[^[:space:]]' | \
+      sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | \
+      awk -F '=' '{gsub(/^["'\'']|["'\'']$/, "", $2); data[$1]=$2} END {for (key in data) print key "=" data[key]}')
 # create a backup of the original .env
 cp .env .env.bak
+else 
+mv env.tmp .env.back
+fi
+
 
 consul-template \
     -vault-default-lease-duration=10s \
